a09a81253d158565e9a56e80e3d467fe625cbf63
SyedAhsan24 <syedahsan94040@gmail.com>
2024-08-03 16:55:30 +0500

    use in react js

---
+++ src/App.jsx
@@ -0,0 +1,442 @@
+/** @format */
+
+// /** @format */
+
+// import React from 'react';
+// import './App.css';
+// import jawan from '../src/jawan pakistan image.png';
+// import thirdmainimage from '../src/third main child image.png';
+// import secondimage from '../src/right child image.png';
+// const App = () => {
+//   return (
+//     <>
+//       <div className="nevbar">
+//         <img className="jawanpakistan" src={jawan} alt=""></img>
+//         <div className="line"></div>
+//         <div className="parent">
+//           <div className="childs" style={{ display: 'flex' }}>
+//             <p>Home</p>
+//             <p>About us</p>
+//           </div>
+//           <div className="thirdforposititon">
+//             <div className="forposi">
+//               <div className="position">
+//                 <div className="first">
+//                   <p>Web and Mobile Application </p>
+//                   <p>Development</p>
+//                 </div>
+//                 <div className="second">
+//                   <p>Flutter Application Deveopment</p>
+//                 </div>
+//                 <div className="third">
+//                   <p>Graphic Design and Photo Editing</p>
+//                 </div>
+//                 <div className="fourth">
+//                   <p>Digital and Social Media Marketing</p>
+//                 </div>
+//                 <div className="fifth">
+//                   <p>Blockchain Development</p>
+//                 </div>
+//               </div>
+//               <p>Training</p>
+//               <img
+//                 className="downarrow"
+//                 src="./src/down arrow image.png"
+//                 alt=""
+//               />
+//             </div>
+//             <p className="contactus">Contact us</p>
+//           </div>
+//         </div>
+//       </div>
+//       {
+//         <div className="secondmain">
+//           <div className="left">
+//             <h1>
+//               Introdunction To <br></br> Jawan Pakistan
+//             </h1>
+//             <br />
+//             <p>
+//               The foundation of every state is the education and skills set of
+//               its youth. Jawan Pakistan is such an organization of Pakistan who
+//               is not only working on youth education but also enhancing skills
+//               set in youth either Free of cost or in a very low cost. This
+//               initiative, initially developed by a single person and with almost
+//               no seed capital, promotes entrepreneurship and innovation and
+//               Sustainable Development Goal, which promotes inclusive and
+//               sustainable economic growth.
+//             </p>
+//           </div>
+//           <div className="right">
+//             <img className="secondchildimage" src={secondimage} alt="" />
+//           </div>
+//         </div>
+//       }
+//       {
+//         <div className="thirdmain">
+//           <div className="left">
+//             <h1>
+//               Mission And Vision <br></br> Behind Jawan Pakistan
+//             </h1>
+//             <br />
+//             <p>
+//               Our vision is to bring our students into the 21st century through
+//               innovation and modern technology. To create a better every day
+//               life for every people. This program is not only impacting the
+//               youth in monetary perspective but it is also creating some
+//               deep-rooted, long-term psychological effects e.g. minimizing
+//               frustration among the unemployed youth.
+//             </p>
+//             <p>
+//               <br />
+//               <br />
+//               <br />
+//               Our mission is to provide high quality education that connects
+//               young people with opportunities to transform their lives. The
+//               educated youngsters who remain unemployed gets frustrated,
+//               depressed and disheartened when they do not succeed in getting
+//               their dream jobs. This training reinforces the educated youth to
+//               earn well through freelancing, develop variety of skills through
+//               specified domains and promote entrepreneurial culture that diverts
+//               youths potentials for constructive purposes. This energy diversion
+//               definitely make the youth more confident, self-reliant and
+//               constructive asset for a progressing economy.
+//             </p>
+//           </div>
+//           <div className="right">
+//             <img
+//               className="thirdmainrightimage"
+//               src={thirdmainimage}
+//               alt=""
+//             ></img>
+//           </div>
+//         </div>
+//       }
+//       <div className="fourthmain">
+//         <img
+//           className="siralimughalimage"
+//           src="./src/sir ali mughal pic.jpg"
+//           alt=""
+//         ></img>
+
+//         <div className="forline"></div>
+//         <h1>Muhammad Ali Mughal</h1>
+//       </div>
+//       <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="inline-block w-8 h-8 text-gray-400 mb-8" viewBox="0 0 975.036 975.036">
+//               <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
+//             </svg>
+//     </>
+//   );
+// };
+// export default App;
+
+/** @format */
+
+import React from 'react';
+import './App.css';
+import jawan from '../src/jawan pakistan image.png';
+import thirdmainimage from '../src/third main child image.png';
+import secondimage from '../src/right child image.png';
+import sirali from '../src/sir ali mughal pic.jpg';
+import first from '../src/fifthfirsthildimage.png';
+import second from '../src/flutter.jfif';
+import third from '../src/graphic.jpg';
+import fourth from '../src/digital.png';
+import fifth from '../src/blockchain.jpg';
+import sixth from '../src/firstmidchild.jpg';
+import seventh from '../src/secondmidchild.jpg';
+import eight from '../src/thirdmidchild.jpg';
+import ninth from '../src/fifthmidchild.jpg';
+import tenth from '../src/sixthmidchild.jpg';
+import eleventh from '../src/fourthmidchild.jpg';
+import facebook from '../src/facebook-removebg-preview (1).png';
+import insta from '../src/instaone-removebg-preview.png';
+import inn from '../src/In-removebg-preview.png';
+const App = () => {
+  return (
+    <>
+      <div className="nevbar">
+        <img className="jawanpakistan" src={jawan} alt=""></img>
+        <div className="line"></div>
+        <div className="parent">
+          <div className="childs" style={{ display: 'flex' }}>
+            <p>Home</p>
+            <p>About us</p>
+          </div>
+          <div className="thirdforposititon">
+            <div className="forposi">
+              <div className="position">
+                <div className="first">
+                  <p>Web and Mobile Application </p>
+                  <p>Development</p>
+                </div>
+                <div className="second">
+                  <p>Flutter Application Deveopment</p>
+                </div>
+                <div className="third">
+                  <p>Graphic Design and Photo Editing</p>
+                </div>
+                <div className="fourth">
+                  <p>Digital and Social Media Marketing</p>
+                </div>
+                <div className="fifth">
+                  <p>Blockchain Development</p>
+                </div>
+              </div>
+              <p>Training</p>
+              <img
+                className="downarrow"
+                src="./src/down arrow image.png"
+                alt=""
+              />
+            </div>
+            <p className="contactus">Contact us</p>
+          </div>
+        </div>
+      </div>
+      <div className="secondmain">
+        <div className="left">
+          <h1>
+            Introduction To <br></br> Jawan Pakistan
+          </h1>
+          <br />
+          <p>
+            The foundation of every state is the education and skills set of its
+            youth. Jawan Pakistan is such an organization of Pakistan who is not
+            only working on youth education but also enhancing skills set in
+            youth either Free of cost or in a very low cost. This initiative,
+            initially developed by a single person and with almost no seed
+            capital, promotes entrepreneurship and innovation and Sustainable
+            Development Goal, which promotes inclusive and sustainable economic
+            growth.
+          </p>
+        </div>
+        <div className="right">
+          <img className="secondchildimage" src={secondimage} alt="" />
+        </div>
+      </div>
+      <div className="thirdmain">
+        <div className="left">
+          <h1>
+            Mission And Vision <br></br> Behind Jawan Pakistan
+          </h1>
+          <br />
+          <p>
+            Our vision is to bring our students into the 21st century through
+            innovation and modern technology. To create a better every day life
+            for every people. This program is not only impacting the youth in
+            monetary perspective but it is also creating some deep-rooted,
+            long-term psychological effects e.g. minimizing frustration among
+            the unemployed youth.
+          </p>
+          <p>
+            <br />
+            <br />
+            <br />
+            Our mission is to provide high quality education that connects young
+            people with opportunities to transform their lives. The educated
+            youngsters who remain unemployed gets frustrated, depressed and
+            disheartened when they do not succeed in getting their dream jobs.
+            This training reinforces the educated youth to earn well through
+            freelancing, develop variety of skills through specified domains and
+            promote entrepreneurial culture that diverts youths potentials for
+            constructive purposes. This energy diversion definitely make the
+            youth more confident, self-reliant and constructive asset for a
+            progressing economy.
+          </p>
+        </div>
+        <div className="right">
+          <img
+            className="thirdmainrightimage"
+            src={thirdmainimage}
+            alt=""
+          ></img>
+        </div>
+      </div>
+      <div className="fourthmain">
+        <img className="siralimughalimage" src={sirali} alt=""></img>
+        <div className="forline"></div>
+        <h1>
+          Muhammad <br className="forbr"></br>Ali Mughal
+        </h1>
+        <p>Founder of jawan pakistan</p>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          className="svg-icon"
+          viewBox="0 0 975.036 975.036"
+          style={{ height: '20px', color: 'gray' }}
+        >
+          <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
+        </svg>
+        <div>
+          {' '}
+          <p className="forpara">
+            We aim to be most efficient provider of business process outsourcing
+            services by setting the industry standards for cost and quality of
+            services. Our long term success will be driven by our relentless
+            focus on recruiting and developing the most talented pool of human
+            capital in our industry.
+          </p>
+        </div>
+      </div>
+      <div className="fifthmain">
+        <div className="forahone">
+          {' '}
+          <h1>IT Trainings Offering In Jawan Pakistan</h1>
+        </div>
+        <div className="fifthchild">
+          <div className="fifthfirstchild">
+            <img className="fifthchldimage" src={first} alt="" />
+            <p>
+              Web and Mobile Application <br></br>Development{' '}
+            </p>
+          </div>
+          <div className="fifthfirstchild">
+            <img className="fifthchldimage" src={second} alt="" />
+            <p>
+              Flutter Mobile Aplication <br></br>Development{' '}
+            </p>
+          </div>
+          <div className="fifthfirstchild">
+            <img className="fifthchldimage" src={third} alt="" />
+            <p>Graphic Design and Video Editing</p>
+          </div>
+        </div>
+        <div className="sixthchild">
+          <div className="fifthfirstchild">
+            <img className="fifthchldimage" src={fourth} alt="" />
+            <p>Digital and Social Media Marketing </p>
+          </div>
+          <div className="fifthfirstchild">
+            <img className="fifthchldimage" src={fifth} alt="" />
+            <p>BlockChain Development </p>
+          </div>
+        </div>
+      </div>
+      <div className="seventshmain">
+        <h1>Glimpses Of Sessions Conducted By Jawan Pakistan</h1>
+      </div>
+      <div className="eightmaindiv">
+        <div className="leftseconddiv">
+          <div className="childsfirst">
+            <div className="childfirstdivs">
+              <img
+                style={{ height: '100%', width: '100%' }}
+                src={sixth}
+                alt=""
+              />
+            </div>
+            <div className="childseconddivs">
+              <img
+                style={{ height: '100%', width: '100%' }}
+                src={seventh}
+                alt=""
+              />
+            </div>
+          </div>
+          <div className="big">
+            <img style={{ height: '100%', width: '100%' }} src={eight} alt="" />
+          </div>
+        </div>
+
+        <div className="rightseconddiv">
+          <div className="rightseconddiv">
+            <div className="childsfirst">
+              <div className="childfirstdivs">
+                <img
+                  style={{ height: '100%', width: '100%' }}
+                  src={ninth}
+                  alt=""
+                />
+              </div>
+              <div className="childseconddivs">
+                <img
+                  style={{ height: '100%', width: '100%' }}
+                  src={tenth}
+                  alt=""
+                />
+              </div>
+            </div>
+            <div className="bigforonetwo">
+              <img
+                style={{ height: '100%', width: '100%' }}
+                src={eleventh}
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="ninthdiv">
+        <div className="form">
+          <h1>Contact</h1>
+          <div className="forjustify">
+            <div className="forflex">
+              <p>
+                Name <span>*</span>
+              </p>
+              <input className="firstinpt" type="text" />
+            </div>
+            <div className="forflex">
+              <p>
+                Email <span>*</span>
+              </p>
+              <input
+                placeholder="syedahsan@gmail.com"
+                className="firstinpt"
+                type="text"
+              />
+            </div>
+            <div className="forflex">
+              <p>
+                Phone Number <span>*</span>
+              </p>
+              <input
+                placeholder="035534239861"
+                className="firstinpt"
+                type="text"
+              />
+            </div>
+            <div className="forflex">
+              <p>
+                Phone Number <span>*</span>
+              </p>
+              <textarea name="" id=""></textarea>
+              <button>Sumbit</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="footer">
+        <img className="jawanpakistan" src={jawan} alt=""></img>
+
+        <div className="forllineone">
+          <h4>Company</h4>
+          <p>Home</p>
+          <p>AAbout Us</p>
+          <p>Trainings</p>
+          <p>Contact Us</p>
+        </div>
+        <div className="forlinetwo">
+          <h4>Contact Details</h4>
+          <p>Muhammad Ali Mughal</p>
+          <p>+9276762762762</p>
+          <p>Jawantechpk@gmail.com</p>
+        </div>
+      </div>
+      <div className="lastfoter">
+        <div className="lastone">
+          <p>@ 2022 JawanPakistan </p>
+        </div>
+        <div className="lasttwo">
+          <img src={facebook} alt="" />
+          <img src={insta} alt="" />
+          <img src={inn} alt="" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
